3e7e87e4cbf3d5c1cd0bf3c99edcc3dcea737ec9
fantasylee <819345985@qq.com>
2020-09-16 16:03:52 +0800

    所有的文件
    所有的文件

---
+++ fun.py
@@ -0,0 +1,99 @@
+from PyQt5 import QtCore, QtGui, QtWidgets
+from selenium import webdriver
+import time,os,configparser,requests
+
+import sys, os, time
+import configparser
+
+
+class Config(object):
+    def __init__(self, path):
+        self.path = path
+        self.cf = configparser.ConfigParser()
+        self.cf.read(self.path)
+
+    def get(self, field, key):
+        try:
+            result = self.cf.get(field, key)
+        except:
+            result = ""
+        return result
+
+    def set(self, filed, key, value):
+        try:
+            self.cf.set(filed, key, value)
+            with open(self.path, 'w') as f:
+                self.cf.write(f)
+        except:
+            pass
+        return True
+    def startConfig(self):
+        config = Config(self.path)
+        iniPath=config.get("downFilePath", "path")
+        value=os.path.isdir(iniPath)
+        if value :
+            return iniPath
+        else:
+            cwdpath=os.getcwd()
+            config.set("downFilePath", "path",cwdpath)
+            return cwdpath
+def search_163music(dr,word):#获取网易云搜索列表结果
+    search_url = "https://music.163.com/#/search/m/?s={word}".format(word=word)
+    # option = webdriver.ChromeOptions()
+    # option.add_argument("headless")
+    # dr=webdriver.Chrome(options=option,executable_path="chromedriver.exe")
+    dr.get(search_url)
+    time.sleep(0.5)
+    dr.switch_to.frame("g_iframe")
+    searchResultPk=dr.find_element_by_class_name("srchsongst").find_elements_by_xpath("./div")
+    result_pack=[]
+    for result in searchResultPk:
+        songInfo=result.find_elements_by_xpath("./div")
+        songNameInfo=(songInfo)[1]#每个div下面有五个a标签，第2,4,5分别为歌曲名，作者名，专辑名(有部分歌曲没有songAlbumUrl，报错)
+        songName,songNameUrl=songNameInfo.text,songNameInfo.find_element_by_tag_name("a").get_attribute("href")
+        songArtistInfo=songInfo[3]
+        songArtistName_source=songArtistInfo.text
+        songArtistName=songArtistName_source.replace("/","and")
+        songAlbumInfo=songInfo[4]
+        songAlbumName = songAlbumInfo.text
+        songTime=result.find_elements_by_xpath("./div")[-1].text
+        songId=songNameUrl.split("id=")[-1]#分割url字符串，获取songID
+        result_pack.append((songName,songArtistName,songAlbumName,songTime,songId))
+    [Config("config.ini").set("songId",str(i),result_pack[i][-1]) for i in range(len(result_pack))]#将获取的songId加到config.ini文件中
+    [Config("config.ini").set("songName",str(i),result_pack[i][0]+"+"+ result_pack[i][1]) for i in range(len(result_pack))]#将获取的songName加到config.ini文件中
+    return result_pack
+def checkFile(path,resourceFileName):
+    for root, dirs, files in os.walk(path):
+        while resourceFileName+".mp3" in files:
+            resourceFileName += "_1"
+    return resourceFileName
+
+
+def songDownload(songName,songId,path):#下载音乐
+    songDownLink="http://music.163.com/song/media/outer/url?id={muId}".format(muId=songId)
+    header={
+    'User-Agent':'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36 QIHU 360SE'
+}
+    songNameF=checkFile(path,songName)
+    if songName == songNameF:
+        isRepeat = False
+    else:
+        isRepeat = True
+    print("start to download "+songName)
+    res=requests.get(songDownLink,headers=header).content
+    with open(path+"/"+songNameF+".download","wb") as f:
+        try:
+            f.write(res)
+            isSuccess = True
+        except:
+            isSuccess =  False
+    if isSuccess:
+        os.rename(path+"/"+songNameF+".download",path+"/"+songNameF+".mp3")
+    return isSuccess,isRepeat,songNameF
+
+
+
+if __name__ == '__main__':
+    # search_163music("周杰伦"
+    # songDownload("红色高跟鞋+蔡健雅","208902","D:/mine/MusicDownload/Demo")
+    pass
---
+++ main.py
@@ -0,0 +1,108 @@
+from PyQt5.QtWidgets import QFileDialog
+from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtCore import pyqtSignal
+from PyQt5.QtWidgets import *
+from downloadMusicUI import *
+from fun import *
+import threading
+import os
+
+
+class MyWindow(QMainWindow, Ui_MainWindow):
+    # 自定义信号
+    mySignal = pyqtSignal(str)
+    def __init__(self, parent=None):
+        super(MyWindow, self).__init__(parent)
+        self.setupUi(self)
+        option = webdriver.ChromeOptions()
+        option.add_argument("headless")
+        # dr=webdriver.Chrome(options=option,executable_path="chromedriver.exe")
+        self.dr=webdriver.Chrome(options=option,executable_path="chromedriver.exe")
+        # self.dr = webdriver.Chrome(options=(webdriver.ChromeOptions()).add_argument("headless"),executable_path="chromedriver.exe")
+    def setupUi(self,MainWindow,parent=None):
+        config=Config("config.ini")
+        super(MyWindow,self).setupUi(MainWindow)
+        self.showFilePath(config.startConfig())
+        self.filePathEdit.setOpenExternalLinks(True)
+        self.filePathEdit.setToolTip("点击打开文件夹")
+        # self.filePathEdit.setStyleSheet("font:20pt '楷体';border-width: 1px;border-style: solid;border-color: rgb(255, 0, 0);")
+        # self.downResultLable.setStyleSheet("font:20pt '宋体';border-width: 1px;border-style: solid;border-color: rgb(255, 0, 0);")
+        self.searchButton.clicked.connect(self.searchSong_Thread1)
+        self.selectPathButton.clicked.connect(self.filePathGet)
+        #设置点击列表后的动作
+        self.resultWidget.doubleClicked.connect(self.doubleClickTableDownload)
+        #todo 优化1 表格填满窗口
+        #水平方向标签拓展剩下的窗口部分，填满表格
+        self.resultWidget.horizontalHeader().setStretchLastSection(True)
+        # 水平方向，表格大小拓展到适当的尺寸
+        self.resultWidget.horizontalHeader().setSectionResizeMode(QtWidgets.QHeaderView.Stretch)
+        # 设置点击选中一行
+        self.resultWidget.setSelectionBehavior(QTableWidget.SelectRows)  # 设置选中行
+        # 设置表格不能被编辑
+        self.resultWidget.setEditTriggers(QTableWidget.NoEditTriggers)
+
+    def searchSong_Thread1(self):
+        self.resultWidget.setRowCount(0)
+        searchValue = self.searchEdit.text()
+        # searchThread = threading.Thread(target=search_163music,args=(searchValue,))
+        all_result=search_163music(self.dr,searchValue)
+        # print(all_result)
+        for i in range(len(all_result)):
+            item = all_result[i]
+            row = self.resultWidget.rowCount()
+            self.resultWidget.insertRow(row)
+            for j in range(len(item)):
+                item = QTableWidgetItem(str(all_result[i][j]))
+                self.resultWidget.setItem(row, j, item)
+    # def isNotClicked_Thread2(self):
+    #     self.searchButton.setEnabled(False)
+    #     for secends in range(20,-1):
+    #         self.searchButton.setText(QtCore.QCoreApplication.translate("MainWindow", secends))
+    #         time.sleep(1)
+    #     self.searchButton.setEnabled(True)
+    #     self.searchButton.setText(QtCore.QCoreApplication.translate("MainWindow", "搜索"))
+
+    def filePathGet(self):
+        config=Config("config.ini")
+        filePath = QFileDialog.getExistingDirectory(self,'open file', './')##选择文件夹
+        config.set("downFilePath","path",filePath)
+        self.showFilePath(config.startConfig())
+    def doubleClickTableDownload(self):
+        config=Config("./config.ini")
+        rowNum=self.resultWidget.currentRow()
+        songId=config.get("songId",str(rowNum))
+        songName=config.get("songName",str(rowNum))
+        downpath=config.get("downFilePath","path")
+        isSuccess,isRepeat,songNameF= songDownload(songName,songId,downpath)
+        if isSuccess:
+            if isRepeat:
+                self.downResultLable.setText("download success!! {a} is existed,it was renamed {b} ".format(a=songName,b=songNameF))
+            else:
+                self.downResultLable.setText(
+                    "{b} download success!! ".format(b=songNameF))
+        else:
+            self.downResultLable.setText(
+                "{b} download failed!! ".format(b=songNameF))
+
+    def showFilePath(self,path):
+        self.filePathEdit.setText("<a href={p}>{p}</a>".format(p=path))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    import sys
+    app = QApplication(sys.argv)
+    myWin = MyWindow()
+    myWin.show()
+    sys.exit(app.exec_())
