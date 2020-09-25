4b5e6534778658501ae7edc65ed6a7ebab203b33
fantasylee <819345985@qq.com>
2020-09-25 15:43:59 +0800

    将搜索结果以DIC保存

--- fun.py
+++ fun.py
@@ -1,6 +1,6 @@
 from PyQt5 import QtCore, QtGui, QtWidgets
 from selenium import webdriver
-import time,os,configparser,requests
+import time,os,configparser,requests,json
 
 import sys, os, time
 import configparser
@@ -37,6 +37,12 @@ class Config(object):
             cwdpath=os.getcwd()
             config.set("downFilePath", "path",cwdpath)
             return cwdpath
+
+class SetJson(object):
+    def __init__(self,path):
+        self.path = path
+
+
 def getResult(dr):
     dr.switch_to.frame("g_iframe")
     searchResultPk = dr.find_element_by_class_name("srchsongst").find_elements_by_xpath("./div")
@@ -58,6 +64,33 @@ def getResult(dr):
     [Config("config.ini").set("songName", str(i), resultPack[i][1] + "-" + resultPack[i][0]) for i in
      range(len(resultPack))]  # 将获取的songName加到config.ini文件中
     return resultPack
+def getDicResult(dr):
+    dr.switch_to.frame("g_iframe")
+    searchResultPk = dr.find_element_by_class_name("srchsongst").find_elements_by_xpath("./div")
+    resultPack = []
+    for result in searchResultPk:
+        songInfoDic={}
+        songInfo = result.find_elements_by_xpath("./div")
+        songNameInfo = (songInfo)[1]  # 每个div下面有五个a标签，第2,4,5分别为歌曲名，作者名，专辑名(有部分歌曲没有songAlbumUrl，报错)
+        songName, songNameUrl = songNameInfo.text, songNameInfo.find_element_by_tag_name("a").get_attribute("href")
+        songArtistInfo = songInfo[3]
+        songArtistName_source = songArtistInfo.text
+        songArtistName = songArtistName_source.replace("/", "+")
+        songAlbumInfo = songInfo[4]
+        songAlbumName = songAlbumInfo.text
+        songTime = result.find_elements_by_xpath("./div")[-1].text
+        songId = songNameUrl.split("id=")[-1]  # 分割url字符串，获取songID
+        songInfoDic["songName"] = songName
+        songInfoDic["songNameUrl"] = songNameUrl
+        songInfoDic["songArtistName"] = songArtistName
+        songInfoDic["songAlbumName"] = songAlbumName
+        songInfoDic["songTime"] = songTime
+        songInfoDic["songId"] = songId
+        songInfoDic["songName"] = songName
+        resultPack.append(songInfoDic)
+    with open("searchResult.json","w",encoding="utf-8") as file:
+        json.dump(resultPack,file,ensure_ascii=False)
+    # return resultPack
 
 def search_163music(dr,word):#获取网易云搜索列表结果
     search_url = "https://music.163.com/#/search/m/?s={word}".format(word=word)
@@ -100,3 +133,8 @@ if __name__ == '__main__':
     # search_163music("周杰伦"
     # songDownload("红色高跟鞋+蔡健雅","208902","D:/mine/MusicDownload/Demo")
     pass
+    dr = webdriver.Chrome()
+    search_url = "https://music.163.com/#/search/m/?s={word}".format(word="五月天")
+    dr.get(search_url)
+    time.sleep(0.5)
+    result=getDicResult(dr)
